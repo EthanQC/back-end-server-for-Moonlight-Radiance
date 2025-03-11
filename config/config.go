34f770ef8203663f5d2bbdcb9d3aac665054d5a6
EthanQC <13537821092@163.com>
2025-03-11 22:59:23 +0800

    考虑编写详细的需求文档

--- config/config.go
+++ config/config.go
@@ -1,1 +1,19 @@
+// config/config.go
 package config
+
+type Config struct {
+	Database struct {
+		DSN string
+	}
+	Redis struct {
+		Addr     string
+		Password string
+		DB       int
+	}
+	JWT struct {
+		Secret string
+	}
+	Server struct {
+		Port string
+	}
+}
