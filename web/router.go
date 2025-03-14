acd1174cf4f02ff62d0c93c57114eeb502ade914
EthanQC <13537821092@163.com>
2025-03-14 17:52:20 +0800

    删除无用的游戏路由

--- web/router.go
+++ web/router.go
@@ -3,7 +3,6 @@ package web
 import (
 	"net/http"
 
-	"github.com/EthanQC/back-end-server-for-Moonlight-Radiance/pkg/game"
 	"github.com/EthanQC/back-end-server-for-Moonlight-Radiance/pkg/user"
 	"github.com/gin-gonic/gin"
 )
@@ -23,20 +22,6 @@ func SetupRouter() *gin.Engine {
 		userGroup.POST("/register", user.RegisterHandler)
 		userGroup.POST("/login", user.LoginHandler)
 		// 其他与用户相关的接口
-	}
-
-	// 游戏相关路由
-	gameGroup := r.Group("/game")
-	{
-		// 简单示例：获取随机卡牌
-		gameGroup.GET("/card/random", func(c *gin.Context) {
-			dummyUser := &user.User{ID: 1, Username: "TestUser"}
-			card := game.CardEngine(dummyUser)
-			c.JSON(http.StatusOK, gin.H{
-				"card": card,
-			})
-		})
-		// 其他与游戏相关的接口
 	}
 
 	return r
